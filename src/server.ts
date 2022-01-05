81708eafb263d88fa29891c7ce3b1588f8196e5d
JoaoGuiBC <joaoguibc@gmail.com>
2022-01-05 02:18:45 -0300

    Setted code linter

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_, response) => {
-  return response.json({message: "Hello World"});
-})
+  return response.json({ message: 'Hello World' });
+});
 
 app.listen(3333, () => console.log('Server started'));
